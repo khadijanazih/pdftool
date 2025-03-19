0a6907afa80d6baa395e0f63123d62c50640aa05
khadijanazih <khadijanazih@gmail.com>
2025-03-19 17:20:07 +0000

    BT and MT Done using new splitting logic

--- functions.py
+++ functions.py
@@ -1,4 +1,4 @@
-import pdfplumber,glob, os,re
+import pdfplumber,glob, os,re,json
 def rename_file(file, new_name):
     dirpath = os.path.dirname(file)
     new_path = os.path.join(dirpath, new_name+".pdf")
@@ -24,15 +24,14 @@ def process_files_in_folder(folder):
     folder_count = len(pdf_files)
     for file in pdf_files:
         print(f"Processing: {file}")  # Debugging line
-        print(get_file_data(file))  # Call your function on each file
+        process = get_file_data(file)  # Call your function on each file
+        print(json.dumps(process, indent=4))
     return folder_count
 
 def get_file_data(file):
     file_data = {}
     pdf = pdfplumber.open(file)
     text = pdf.pages[0].extract_text().splitlines()
-    for val in text :print(f"{text.index(val)} # {val}")
-
     pdf.close()
     new_name = ""
     tension = ""
@@ -42,21 +41,30 @@ def get_file_data(file):
         return
     if file_type(file) == "BT":
         try:
-            file_data['facture'] = text[10].split(" ")[3]
+            lineind, textind = locate_text(text, "Facture")
+            file_data['facture'] = text[lineind].split()[textind + 3]
         except ValueError:
             print("'N° facture' is not in the list")
             new_name ="not compatible"
             rename_file(file, new_name)
             return
         tension = "BT"
-        file_data['contrat_SAP'] = text[5].split(" ")[text[5].split(" ").index("Contrat") + 1]
-        file_data['contrat_Waterp'] = text[5].split(" ")[text[5].split(" ").index("Contrat")+3]
-        file_data['N° Client'] = text[1].split(" ")[2]
-        file_data['Nom Client'] = " " .join(text[2].split()[1:-1])
 
-    elif file_type(file):
+        lineind, textind = locate_text(text, "Contrat")
+
+        file_data['contrat_SAP'] = text[lineind].split()[textind + 1]
+        file_data['contrat_Waterp'] = text[lineind].split()[textind + 3]
+
+        lineind, textind = locate_text(text, "N°")
+        file_data['N° Client'] = text[lineind].split()[textind + 2]
+
+        lineind, textind = locate_text(text, "Client")
+        file_data['Nom Client'] = " ".join(text[lineind+1].split()[textind:-1])
+
+    elif file_type(file) == "MT":
         try:
-            file_data['facture'] = text[3].split(" ")[1]
+            lineind, textind = locate_text(text, "N°")
+            file_data['facture'] = text[lineind].split()[textind + 1]
         except ValueError:
             print("'N° facture' is not in the list")
             new_name ="not compatible"
@@ -64,16 +72,16 @@ def get_file_data(file):
             return
         tension = "MT"
         file_data['Nom Client'] = text[0]
-        file_data['contrat_Waterp'] = text[5].split(" ")[4]
-        file_data['contrat_SAP'] = text[5].split(" ")[2]
-        file_data['N° Client'] = text[8].split(" ")[2]
+        lineind, textind = locate_text(text, "Contrat")
+        file_data['contrat_SAP'] = text[lineind].split()[textind + 2]
+        file_data['contrat_Waterp'] = text[lineind].split()[textind + 4]
+        lineind, textind = locate_text(text, "Client")
+        file_data['N° Client'] = text[lineind].split()[textind + 2]
 
     new_name = file_data['Nom Client'] + " - " + file_data['facture']+ " " + tension
     rename_file(file, new_name)
     print("#####################################")
     return file_data
-
-
 def file_type(file):
     pdf = pdfplumber.open(file)
     text = pdf.pages[0].extract_text().split(" ")
@@ -94,3 +102,25 @@ def process_mt(file):
     file_data['contrat_SAP'] = text[5].split(" ")[2]
     file_data['N° Client'] = text[8].split(" ")[2]
     return file_data
+
+def locate_text(text_split_array, text):
+    line_index = -1
+    text_index = -1
+    for line in text_split_array:
+        line_split = line.split(" ")
+        if text in line:
+            line_index = text_split_array.index(line)
+            text_index = line_split.index(text)
+            break
+    return line_index, text_index
+
+def test_function(folder):
+    pdf_files = glob.glob(os.path.join(folder, "*.pdf"))  # Get all PDF files in the folder
+    for file in pdf_files:
+        if pdf_files.index(file)==10:break
+        text = pdfplumber.open(file).pages[0].extract_text().splitlines()
+        print(f"##### Processing: {file.split("\\")[-1].split(".")[0]} #####: ")
+        lineind, textind = locate_text(text, "Client")
+        print()
+
+        print("------------------------------------------------------------")
--- setup.py
+++ setup.py
@@ -1,8 +1,12 @@
+
+
 import functions as fu
 import pdftool_gui as gui
 
 FOLDER = gui.folder
 print(fu.process_files_in_folder(FOLDER))
+
+
 
 
 
